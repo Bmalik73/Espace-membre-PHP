8a08791e32aa4811733427d04d8a8332e49f098d
Bmalik73 <bmalik01@live.fr>
2023-07-21 14:48:06 +0200

    Update register.php

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
     <title>Page d'accueil</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
-<body>
+<body style="background: #000000; color:#fff;">
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
@@ -34,29 +34,34 @@
             </div>
         </div>
     </nav>
-    <div class="col-md-8 col-md-offset-2">
-        <form action="" method="post">
-            <fieldset>
-                <div class="form-group">
-                    <label for="pseudo">Nom d'utilisateur</label>
-                    <input type="text" id="pseudo" class="form-control" name="username">
-                </div>
-                <div class="form-group">
-                    <label for="Email">Email</label>
-                    <input type="email" id="email" class="form-control" name="email">
-                </div>
-                <div class="form-group">
-                    <label for="password">Mot de Passe</label>
-                    <input type="password" id="password" class="form-control" name="password">
-                </div>
-                <div class="form-group">
-                    <label for="password">Confirmation du Mot de Passe</label>
-                    <input type="password" id="password" class="form-control" name="password_confirm">
-                </div>
-                <input type="submit" class="btn btn-primary" value="S'inscrire">
-            </fieldset>
-        </form> 
+    <div class="container">
+        <div class="col-md-8 col-md-offset-2" >
+            <h1 style="color:#fff;">S'inscrire</h1>
+
+            <form action="" method="post">
+                <fieldset>
+                    <div class="form-group">
+                        <label for="pseudo">Nom d'utilisateur</label>
+                        <input type="text" id="pseudo" class="form-control" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="Email">Email</label>
+                        <input type="email" id="email" class="form-control" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Mot de Passe</label>
+                        <input type="password" id="password" class="form-control" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Confirmation du Mot de Passe</label>
+                        <input type="password" id="password" class="form-control" name="password_confirm">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="S'inscrire">
+                </fieldset>
+            </form> 
+        </div>
     </div>
+    
 
 </body>
 </html>
